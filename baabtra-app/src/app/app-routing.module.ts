09fe00a18ab645e1f22e5f4f9b2ba0d9faa32e7b
Sree <sreepriya5155@gmail.com>
2022-11-07 20:43:29 +0400

    add bootstrap and routing

--- baabtra-app/src/app/app-routing.module.ts
+++ baabtra-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActivityComponent } from './activity/activity.component';
+import { CourseComponent } from './course/course.component';
+import { MainComponent } from './main/main.component';
+import { PlacementComponent } from './placement/placement.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path:'',redirectTo:'About',pathMatch:'full'},
+  {path:'About',component:MainComponent},
+  {path:'Service',component:CourseComponent},
+  {path:'Activity',component:ActivityComponent},
+  {path:'Placement',component:PlacementComponent},
+  //{path:'**',component:}//wildcard rout (the page will not present)
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
